db13a659289e8e24ebcca896082f30a4cc5b8067
kunal <jindalkunal06@gmail.com>
2024-05-08 17:31:17 +0530

    njd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import React from 'react';
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { ClerkProvider } from "@clerk/nextjs";
